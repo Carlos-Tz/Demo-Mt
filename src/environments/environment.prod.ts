a03c63258ed1407cca5d004f446e72b0ce7af95c
Charly921 <44937626+Charly921@users.noreply.github.com>
2020-05-19 15:33:31 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    /* apiKey: 'AIzaSyDRKTv0t1MUnXZ0kgjBt6Yk6_V9I81qNzw',
+    authDomain: 'mt-ingenieria-d6a3e.firebaseapp.com',
+    databaseURL: 'https://mt-ingenieria-d6a3e.firebaseio.com',
+    projectId: 'mt-ingenieria-d6a3e',
+    storageBucket: 'mt-ingenieria-d6a3e.appspot.com',
+    messagingSenderId: '716976786737',
+    appId: '1:716976786737:web:abbca71b25ad2f40503410' */
+    apiKey: 'AIzaSyDHONQoYq7DHl0VZZhiCfM1LoLtBL6wiSM',
+    authDomain: 'mt-ingenieria.firebaseapp.com',
+    databaseURL: 'https://mt-ingenieria.firebaseio.com',
+    projectId: 'mt-ingenieria',
+    storageBucket: 'mt-ingenieria.appspot.com',
+    messagingSenderId: '377697561594',
+    appId: '1:377697561594:web:b0cde238d4aaa541'
+  }
 };
